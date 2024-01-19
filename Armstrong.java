2f132bd3ef786d30cec2cfe3abf7e1a56342ca57
Dakoji-Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-01-19 14:21:10 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,22 @@
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=153;
+		int temp,r,rev=0;
+		temp=n;
+		while(n!=0) {
+			r = n % 10;
+			rev = rev + (r*r*r);
+			n = n / 10;
+		}
+		System.out.println(rev);
+		if (rev==temp) {
+			System.out.println("Amrstrong");
+		}else {
+			System.out.println(" Not Amrstrong");
+		}
+	}
+
+}
